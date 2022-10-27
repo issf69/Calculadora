1ca65f8834d14927a6a4c8f5fd2ba433c1b573b5
Irene Silva França <105497075+issf69@users.noreply.github.com>
2022-10-27 10:07:02 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,29 @@
+public class Calculadora {
+    /**
+     * <h1>Calculadora</h1>
+     * A calculadora realiza operações matemáticas entre números
+     * <P>
+     * <b>Note:</b> Leia atentamente a documentação desta classe
+     * para desfrutar dos recursos oferecidos pelo autor
+     *
+     * @ author Irene Silva
+     * @ version 1.0
+     * 
+     * @since 01/01/2022
+     */
+
+    public class calculadora {
+        /**
+         * Este método é ulilizado para somar dois números inteiros inteiros
+         * 
+         * @param numeroUm   este é o primeiro parâmetro do método.
+         * @param numeroDois este é o segundo parâmetro do método.
+         * @return int o resultado deste método é a soma dos dois números.
+         */
+        public int somar(int numeroUm, int numeroDois) {
+            return numeroUm + numeroDois;
+        }
+
+    }
+
+}
